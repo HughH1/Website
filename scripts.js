06bfa58423e6d31a3627f162d11c0d9de5f8dc33
HughH1 <hughfharding@gmail.com>
2019-06-05 17:30:57 +0100

    small change

--- scripts.js
+++ scripts.js
@@ -28,7 +28,6 @@ function theInput() {
     sendData(myAccount, "http://localhost:8080/JavaEEServer-1.0/api/accounts/");
     console.log(myAccount);
     // document.getElementById("inputName").innerText = JSON.stringify(inp);
-
 }
 
 
@@ -50,20 +49,3 @@ function sendData(data, url) {
         req.send(data);
     })
 }
-
-
-
-function sendReq() {
-    fetchData("https://raw.githubusercontent.com/ewomackQA/JSONDataRepo/master/example.json").then((req) => {
-        let superHeroObject = JSON.parse(req.responseText);
-        console.log(req.responseText);
-        console.log(superHeroObject);
-
-
-    });
-}
-
-
-
-
-
